05fbe8a3bcbc702f0b97a20ae3f352b6ca517184
Dinesh <dinesh.punni@hotmail.de>
2017-09-24 18:16:48 +0200

    New Project added ( works with voice rec now)

---
+++ Assets/Scripts/CustomCarMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+public class CustomCarMovement : MonoBehaviour {
+
+	public CarUserControl carUsrCtrl;
+
+	void Start () {
+
+		carUsrCtrl = GetComponent<CarUserControl>();
+	}
+	
+	void Update () {
+
+
+
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			transform.Rotate(0, 90, 0);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			transform.Rotate(0, -90, 0);
+		}
+
+		if (Input.GetKeyDown(KeyCode.F))
+		{
+			carUsrCtrl.startMovingForward = true;
+		}
+
+	}
+
+
+	void OnStartMoving()
+	{
+		carUsrCtrl.startMovingForward = true;
+	}
+}
